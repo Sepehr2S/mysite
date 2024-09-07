3935bf143f2ea7e6488650f2cfea0b1ea12e9727
Sepehr2S <sepehrsaeedizadeh@gmail.com>
2024-09-07 10:18:58 +0330

    admin and tables
    create and customizing admin panel, creating tables

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    search_fields = ("title", "content",)
+    list_display = ("title", "status" , "created_date", "counted_views", )
+    ordering= ("-created_date",)
+    empty_value_display = ("-empty-",)
+    list_filter = ("status",)
+
+admin.site.register(Post, PostAdmin)
