39620cfd97b633d21923421a1af61793b7400ef6
Sepehr2S <sepehrsaeedizadeh@gmail.com>
2024-09-16 10:30:18 +0330

    fixing categories

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator
 
 def blog_view(request):
     posts = Post.objects.filter(status = 1)
-    context = {'posts': posts}
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'posts': page_obj}
     return render(request, "blog/blog-home.html", context)
     
 def blog_single(request, pid):
-    posts = get_object_or_404(Post, pk=pid)
+    posts = get_object_or_404(Post, pk=pid) 
     context = {'posts': posts}
     return render(request, "blog/blog-single.html", context)
 
