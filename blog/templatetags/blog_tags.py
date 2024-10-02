50659c6aafa9aa12aefa60b711c7466aef3cbb4a
Sepehr2S <sepehrsaeedizadeh@gmail.com>
2024-10-02 16:19:09 +0330

    basic simple tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,11 +3,13 @@ from blog.models import Post
 
 register = template.Library()   
 
-@register.simple_tag
-def totalpost():
+@register.simple_tag(name="totalposts")
+def function():
+    posts = Post.objects.filter(status=1).count()
+    return posts
+    
+    
+@register.simple_tag(name="posttitle")
+def function():
     posts = Post.objects.filter(status=1)
-    total = 0
-    for i in posts:
-        if i:
-            total += 1
-    return total
+    return posts
