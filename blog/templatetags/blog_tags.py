e80dba62c8a7ddfb07a384da64bef93d59809a8f
Sepehr2S <sepehrsaeedizadeh@gmail.com>
2024-10-02 15:20:30 +0330

    few updates

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,13 @@
 from django import template
+from blog.models import Post
 
-register = template.Library()
+register = template.Library()   
+
+@register.simple_tag
+def totalpost():
+    posts = Post.objects.filter(status=1)
+    total = 0
+    for i in posts:
+        if i:
+            total += 1
+    return total
